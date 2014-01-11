cb510b51b88083c6f7a9fb623d07d25fda1d9bf6
Paul Land <paul.land94@gmail.com>
2014-01-10 17:45:40 -0800

    Updated Run.py

--- Run.rb
+++ Run.rb
@@ -10,9 +10,9 @@ require './app/Analyzer'
 require './app/Poster'
 require './app/data/bounding_boxes'
 
-json_file = "SampleTweets.json"
-tweets = load(json_file, "")
-raw_text = get_raw_text(tweets)
-en_lang_tweets = remove_non_en_lang(tweets)
-no_urls_text = remove_urls(tweets)
-processed_text = process(no_urls_text)
+$json_file = "SampleTweets.json"
+$tweets = load($json_file, "")
+$raw_text = get_raw_text($tweets)
+$en_lang_tweets = remove_non_en_lang($tweets)
+$no_urls_text = remove_urls($tweets)
+$processed_text = process($no_urls_text)
